c7fe076677f1acf9f8fd0f8db594c90dac0ffd29
Irangaumayantha2001 <iamiranga9@gmail.com>
2023-03-21 22:51:32 +0530

    add components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import About from "./components/About";
+import AllnOne from "./components/AllnOne";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Pricing from "./components/Pricing";
+import Support from "./components/Support";
 
 
 function App() {
   return (
     <div className="App">
-         <h1 > Hi</h1>
+         <Navbar/>
+         <Hero/>
+         <AllnOne/>
+         <Pricing/>
+         <About/>
+         <Support/>
+         <Footer/>
     </div>
   );
 }
---
+++ src/components/AllnOne.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AllnOne() {
+  return (
+    <div>AllnOne</div>
+  )
+}
+
+export default AllnOne
